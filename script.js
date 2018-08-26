e7cddd2b4a30c23b792b58fc2608b3a566fd0b81
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-26 23:38:20 +0000

    .empty

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
 
-button[0].oneclick = function() {
-  
-    if(wrapped) {
-        $("section").unwrap();
-        wrapped = false;
-        button.text("wrap");
-    } 
-    
-    else
-    
-    {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("unwrap");
-    }
-};
+$(".button").empty();
+$("#points-of-sale").empty();
+
+
+
+
+/*
+.empty() - empties the inner HTML of an element
+.remove() - removes the element completely
+*/
