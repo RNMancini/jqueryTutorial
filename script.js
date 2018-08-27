7d8272023daf0188f707aea77c25dc8e26988ef7
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-27 21:59:46 +0000

    remove class

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-$("#social-nav").css( {
-    "top": "-400px",
-    "left": "150px",
-    "opacity" : "0.5",
-    "border-top" : "4px solid red"
-});
+$("header .wrapper").removeClass("wrapper")
+
+/*
+removeClass() - removes a class from the matched element(s)
+addClass() - adds a class to the matched element(s)
+toggleClass - toggles the class on and off on their matched element(s)
+*/
