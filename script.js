2ab890c5e3a9d0d1322ae10833e10f5215be4fd4
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-27 23:45:17 +0000

    Lesson 17 .ready

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-var button = $("#lead-banner a");
-
-button[0].onclick = function() {
-    $("#points-of-sale").toggleClass("open");
-    return false;
-};
-
-/*
-removeClass() - removes a class from the matched element(s)
-addClass() - adds a class to the matched element(s)
-toggleClass - toggles the class on and off on their matched element(s)
-=======
-
-var myLis = $('#point-of-sale li');
-
-myLis.on("click", function(){
+$(document).on("ready", function(){
     
-    $(this).css({"background" : "pink"});
-    
-    myLis.off("click")
     
 });
-
-
-
- //lesson16
- 
- $("#lead-banner").dblclick(function() {
-     alert("you double clicked me");
-     $("#lead-banner").off("dblclick");
-    
-});
-
-
-/*
-
-on() - binds an event to matched element(s)
-off() - unbinds event from matched element(s)
-
-
-*/
