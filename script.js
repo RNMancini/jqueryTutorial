c1295498b2c716eb9c6ea434b9cde527b2c22184
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-28 23:29:33 +0000

    branch finished

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     
    
-   $("section > h2").on("click", function() {
+   $("img[alt=map]").on("click", function() {
        
-       $(this).hide(1000).show(1000);
+       $("section > h2").toggle(1000);
        
      /*
      fadeIn(0, fadeOut(), fadeIn()
