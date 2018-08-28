f4d41e2208c1794d4e6bfdb0fadbfee17c71f152
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-28 22:34:55 +0000

    FadeTo

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
    
    $("section > h2").on("click", function() {
        
-       $(this).fadeOut(2000).fadeIn(500);
-      
+       $(this).fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.8);
+                .fadeTo(200, 0.2)
+                .fadeTo(200, 0.8)
        });
        
    });
