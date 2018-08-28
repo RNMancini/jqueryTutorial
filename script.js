f05bd4b5ffed67f141075b934346556c646c05cf
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-28 00:00:35 +0000

    lesson18 branch complete

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     
-    element.on("click", function() {
+    element.on("click", function(e) {
+        
+    });
+    
+});
+
+
+//next step
+$(document).ready(function(){
+    
+    $(".").on("click", function(e) {
+        
+        console.log(e.target);
+        console.log("The event type is: " + e.type);
+       console.log("X co-ordinate of the event is: " + e.pageX);
+       console.log("Y co-ordinate of the event is: " + e.pageY);
+        e.stopPropagation();
         
     });
     
