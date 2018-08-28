b92fe013e89ada325c4ad8074695dfab250f32d5
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-28 22:20:03 +0000

    Branch 19 finished

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
    
    $("section > h2").on("click", function() {
        
-       $(this).animate({"width" : "500px", "height" : "100px"}, 4000, "linear", function(){
-           alert("animation complete");
+       $(this).animate({"width" : "500px", "height" : "100px"}, 4000, "linear", complete);
        });
        
    });
